7ac41b6acb6fc19806010463d372a8352bc833eb
Efim <efim-02@mail.ru>
2020-10-19 01:17:35 +0300

    FV страница каталог

---
+++ catalog.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Главная</title>
+	<link rel="stylesheet" href="/static/css/main.css">
+</head>
+
+<body>
+	<?php require('./parts/header.php'); ?>
+	<main class="main">
+		<div class="main__inner">
+			<div class="main__content">
+				<div class="all-courses">
+					<div class="all-courses__title">Все курсы</div>
+					<div class="all-courses__cards">
+						<div class="all-courses__card course-card">
+							<div class="course-card__image">
+								<img src="./static/img/course_card1.png" alt="...">
+								<div class="course-card__types">
+									<div class="course-card__type type">музыка</div>
+								</div>
+							</div>
+							<div class="course-card__about">
+								<div class="course-card__name">Музыкальный конструктор</div>
+								<div class="course-card__description">Собери 21 музыкальную композицию в разных музыкальных стилях. </div>
+								<div class="course-card__bottom">
+									<div class="course-card__tags">
+										<div class="course-card__tag tag">24 видео</div>
+										<div class="course-card__tag tag">21 композиция</div>
+										<div class="course-card__tag tag">18 часов практики</div>
+										<div class="course-card__tag tag">24 видео</div>
+									</div>
+									<div class="course-card__btn">
+										<div class="btn-send">
+											<object data="./static/img/btn-send.svg" type="image/svg+xml"></object>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="all-courses__card course-card">
+							<div class="course-card__image">
+								<img src="./static/img/course_card2.png" alt="...">
+								<div class="course-card__types">
+									<div class="course-card__type type">дизайн</div>
+								</div>
+							</div>
+							<div class="course-card__about">
+								<div class="course-card__name">Творческий конструктор</div>
+								<div class="course-card__description">Собери 21 музыкальную композицию в разных музыкальных стилях. </div>
+								<div class="course-card__bottom">
+									<div class="course-card__tags">
+										<div class="course-card__tag tag tag_border-red">24 видео</div>
+										<div class="course-card__tag tag tag_border-red">21 композиция</div>
+										<div class="course-card__tag tag tag_border-red">18 часов практики</div>
+										<div class="course-card__tag tag tag_border-red">24 видео</div>
+									</div>
+									<div class="course-card__btn">
+										<div class="btn-send">
+											<object data="./static/img/btn-send.svg" type="image/svg+xml"></object>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="all-courses__card course-card">
+							<div class="course-card__image">
+								<img src="./static/img/course_card3.png" alt="...">
+								<div class="course-card__types">
+									<div class="course-card__type type">фото</div>
+								</div>
+							</div>
+							<div class="course-card__about">
+								<div class="course-card__name">Музыкальный конструктор</div>
+								<div class="course-card__description">Собери 21 музыкальную композицию в разных музыкальных стилях. </div>
+								<div class="course-card__bottom">
+									<div class="course-card__tags">
+										<div class="course-card__tag tag tag_border-violet">24 видео</div>
+										<div class="course-card__tag tag tag_border-violet">21 композиция</div>
+										<div class="course-card__tag tag tag_border-violet">18 часов практики</div>
+										<div class="course-card__tag tag tag_border-violet">24 видео</div>
+									</div>
+									<div class="course-card__btn">
+										<div class="btn-send">
+											<object data="./static/img/btn-send.svg" type="image/svg+xml"></object>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="all-courses__card course-card">
+							<div class="course-card__image">
+								<img src="./static/img/course_card4.png" alt="...">
+								<div class="course-card__types">
+									<div class="course-card__type type">музыка</div>
+								</div>
+							</div>
+							<div class="course-card__about">
+								<div class="course-card__name">Творческий конструктор</div>
+								<div class="course-card__description">Собери 21 музыкальную композицию в разных музыкальных стилях. </div>
+								<div class="course-card__bottom">
+									<div class="course-card__tags">
+										<div class="course-card__tag tag tag_border-green">24 видео</div>
+										<div class="course-card__tag tag tag_border-green">21 композиция</div>
+										<div class="course-card__tag tag tag_border-green">18 часов практики</div>
+										<div class="course-card__tag tag tag_border-green">24 видео</div>
+									</div>
+									<div class="course-card__btn">
+										<div class="btn-send">
+											<object data="./static/img/btn-send.svg" type="image/svg+xml"></object>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+	<?php require('./parts/footer.php'); ?>
+</body>
+
+</html>
