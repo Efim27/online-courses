3a8945cf5aad83dcbf77fd45e4750505c3fffc6d
Efim <efim-02@mail.ru>
2020-11-09 03:18:09 +0300

    +reviews page

--- themes/online_courses/assets/javascript/main.js
+++ themes/online_courses/assets/javascript/main.js
@@ -1,3 +1,6 @@
+//Ограничение по кол-ву символов текст
+var text_limit = 250;
+
 //Обработка нажатий на svg object
 $.fn.svg_click = function (click_func) {
 	this.each(function () {
@@ -7,13 +10,31 @@ $.fn.svg_click = function (click_func) {
 	});
 };
 
-$.fn.toggleAttr = function(attr, attr1, attr2) {
-	return this.each(function() {
+$.fn.toggleAttr = function (attr, attr1, attr2) {
+	return this.each(function () {
 		var self = $(this);
-		if (self.attr(attr) == attr1)
-			self.attr(attr, attr2);
-		else
-			self.attr(attr, attr1);
+		if (self.attr(attr) == attr1) self.attr(attr, attr2);
+		else self.attr(attr, attr1);
+	});
+};
+
+$.fn.text_limit = function () {
+	return this.each(function () {
+		let str = $(this)[0].textContent;
+		let strtemp = str.substr(0, text_limit);
+		str =
+			strtemp +
+			"<span class=\"text-limited__hidden\">" +
+			str.substr(text_limit, str.length) +
+			"</span><span class=\"text-limited__dots\">...</span>";
+		$(this).html(str);
+	});
+};
+
+$.fn.text_unlimit = function () {
+	return this.each(function () {
+		$(this).find('.text-limited__dots').remove();
+		$(this).html($(this)[0].textContent);
 	});
 };
 
@@ -29,51 +50,66 @@ function get_div_by_svg(div_selector, svg) {
 }
 
 function contact_form_success() {
-	alert('Вы успешно отправили нам вопрос');
+	alert("Вы успешно отправили нам вопрос");
 }
 
 $(document).ready(function () {
-
-	$(document).mousedown(function (e){
-		var popup = $('.popup__content');
+	$(document).mousedown(function (e) {
+		var popup = $(".popup__content");
 		if (!popup.is(e.target) && popup.has(e.target).length === 0) {
-			popup.parent().hide('slow');
+			popup.parent().hide("slow");
 		}
 	});
 
-	$('.popup__close').click(function (e) {
-		$(this).parent().parent().parent().hide('slow');
-	});
-
-	$('.signin-link').click(function (e) {
+	$('.text-limited').text_limit();
+	$(".btn-text-limited").click(function (e) {
 		e.preventDefault();
-		$('.popup').hide('slow');
-		$('.popup-signin').show('slow').css('display', 'flex');
+		let parent_div = $(this).parent();
+		$(this).hide('slow');
+		parent_div.find('.text-limited').text_unlimit();
 	});
 
-	$('.signup-link').click(function (e) {
+	$(".popup__close").click(function (e) {
+		$(this).parent().parent().parent().hide("slow");
+	});
+
+	$(".signin-link").click(function (e) {
 		e.preventDefault();
-		$('.popup').hide('slow');
-		$('.popup-signup').show('slow').css('display', 'flex');
+		$(".popup").hide("slow");
+		$(".popup-signin").show("slow").css("display", "flex");
 	});
 
-	$('.faq-stage__more-btn').click(function () {
-		$(this).toggleClass('more-btn_close');
-		$(this).prev().find('.faq-stage__answer').toggle('slow');
+	$(".signup-link").click(function (e) {
+		e.preventDefault();
+		$(".popup").hide("slow");
+		$(".popup-signup").show("slow").css("display", "flex");
+	});
+
+	$(".faq-stage__more-btn").click(function () {
+		$(this).toggleClass("more-btn_close");
+		$(this).prev().find(".faq-stage__answer").toggle("slow");
 	});
 
 	$(window).load(function () {
 		//svg hover
-		$('.svg-link').on('mouseenter', function() {
-			$(this).find('object').get(0).contentDocument.getElementsByTagName('svg')[0].classList.add('hover');
+		$(".svg-link").on("mouseenter", function () {
+			$(this)
+				.find("object")
+				.get(0)
+				.contentDocument.getElementsByTagName("svg")[0]
+				.classList.add("hover");
 		});
 
-		$('.svg-link').on('mouseleave', function() {
-			$(this).find('object').get(0).contentDocument.getElementsByTagName('svg')[0].classList.remove('hover');
+		$(".svg-link").on("mouseleave", function () {
+			$(this)
+				.find("object")
+				.get(0)
+				.contentDocument.getElementsByTagName("svg")[0]
+				.classList.remove("hover");
 		});
 
-		$(".side-menu").addClass('side-menu_loaded');
-		$(".popup").addClass('popup_loaded');
+		$(".side-menu").addClass("side-menu_loaded");
+		$(".popup").addClass("popup_loaded");
 
 		$(".side-menu-btn object").svg_click(function () {
 			$(".side-menu").show("slow");
@@ -87,18 +123,21 @@ $(document).ready(function () {
 			$(".contact-us__form").submit();
 		});
 
-		$('.popup__close object').svg_click(function () {
+		$(".popup__close object").svg_click(function () {
 			let svg = $(this);
-			let object_selector = '.popup__close object';
+			let object_selector = ".popup__close object";
 			let svg_div = get_div_by_svg(object_selector, svg);
-			svg_div.parent().parent().parent().hide('slow');
+			svg_div.parent().parent().parent().hide("slow");
 		});
 
-		$('.text-input__toggle-pswd object').svg_click(function () {
+		$(".text-input__toggle-pswd object").svg_click(function () {
 			let svg = $(this);
-			let object_selector = '.text-input__toggle-pswd object';
+			let object_selector = ".text-input__toggle-pswd object";
 			svg_div = get_div_by_svg(object_selector, svg);
-			svg_div.parent().find('input').toggleAttr('type', 'text', 'password');
+			svg_div
+				.parent()
+				.find("input")
+				.toggleAttr("type", "text", "password");
 		});
 	});
 });
