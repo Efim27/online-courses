f06f5844b44d43b3b60445d13a2847b54747632a
ftp <webpar3z_mir@cartman.beget.ru>
2020-11-10 06:47:45 +0300

    plugin courses add slags

---
+++ plugins/hima/courses/updates/builder_table_update_hima_courses_courses_2.php
@@ -0,0 +1,23 @@
+<?php namespace Hima\Courses\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateHimaCoursesCourses2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('hima_courses_courses', function($table)
+        {
+            $table->string('slug');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('hima_courses_courses', function($table)
+        {
+            $table->dropColumn('slug');
+        });
+    }
+}
