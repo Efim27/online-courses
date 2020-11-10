b407aafdcb6ef11d7c98bcf5b326803ba3c165ac
ftp <webpar3z_mir@cartman.beget.ru>
2020-11-10 06:54:36 +0300

    plugin courses slug auto

---
+++ plugins/hima/courses/updates/builder_table_update_hima_courses_types_2.php
@@ -0,0 +1,23 @@
+<?php namespace Hima\Courses\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateHimaCoursesTypes2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('hima_courses_types', function($table)
+        {
+            $table->renameColumn('slag', 'slug');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('hima_courses_types', function($table)
+        {
+            $table->renameColumn('slug', 'slag');
+        });
+    }
+}
