319ee2bade74da34460ae7b09bb4148f54872a82
Efim <efim-02@mail.ru>
2020-11-12 03:40:29 +0300

    анимация welcome-title

--- themes/online_courses/assets/javascript/main.js
+++ themes/online_courses/assets/javascript/main.js
@@ -24,16 +24,16 @@ $.fn.text_limit = function () {
 		let strtemp = str.substr(0, text_limit);
 		str =
 			strtemp +
-			"<span class=\"text-limited__hidden\">" +
+			'<span class="text-limited__hidden">' +
 			str.substr(text_limit, str.length) +
-			"</span><span class=\"text-limited__dots\">...</span>";
+			'</span><span class="text-limited__dots">...</span>';
 		$(this).html(str);
 	});
 };
 
 $.fn.text_unlimit = function () {
 	return this.each(function () {
-		$(this).find('.text-limited__dots').remove();
+		$(this).find(".text-limited__dots").remove();
 		$(this).html($(this)[0].textContent);
 	});
 };
@@ -53,15 +53,14 @@ function contact_form_success() {
 	alert("Вы успешно отправили нам вопрос");
 }
 
-if ($('.project-review').length > 0) {
+if ($(".project-review").length > 0) {
 	let project_review_slider = new Glide(".project-review", {
-		type: 'carousel',
+		type: "carousel",
 		perView: 1,
-		focusAt: 'center'
-	})
+		focusAt: "center",
+	});
 	project_review_slider.mount();
-}
-else {
+} else {
 	project_review_slider = null;
 }
 
@@ -73,12 +72,12 @@ $(document).ready(function () {
 		}
 	});
 
-	$('.text-limited').text_limit();
+	$(".text-limited").text_limit();
 	$(".btn-text-limited").click(function (e) {
 		e.preventDefault();
 		let parent_div = $(this).parent();
-		$(this).hide('slow');
-		parent_div.find('.text-limited').text_unlimit();
+		$(this).hide("slow");
+		parent_div.find(".text-limited").text_unlimit();
 	});
 
 	$(".popup__close").click(function (e) {
@@ -104,7 +103,11 @@ $(document).ready(function () {
 
 	$(".course-page-block__more-btn").click(function () {
 		$(this).toggleClass("more-btn_close");
-		$(this).parent().parent().find(".course-page-block__content").toggle("slow");
+		$(this)
+			.parent()
+			.parent()
+			.find(".course-page-block__content")
+			.toggle("slow");
 	});
 
 	$(window).load(function () {
@@ -127,13 +130,15 @@ $(document).ready(function () {
 
 		$(".side-menu").addClass("side-menu_loaded");
 		$(".popup").addClass("popup_loaded");
-		$('.project-reviews__btns_mini').addClass("project-reviews__btns_mini-loaded");
+		$(".project-reviews__btns_mini").addClass(
+			"project-reviews__btns_mini-loaded"
+		);
 
-		$('.project-reviews__btn-left object').svg_click(function() {
-			project_review_slider.go('<');
+		$(".project-reviews__btn-left object").svg_click(function () {
+			project_review_slider.go("<");
 		});
-		$('.project-reviews__btn-right object').svg_click(function() {
-			project_review_slider.go('>');
+		$(".project-reviews__btn-right object").svg_click(function () {
+			project_review_slider.go(">");
 		});
 
 		$(".side-menu-btn object").svg_click(function () {
@@ -165,4 +170,62 @@ $(document).ready(function () {
 				.toggleAttr("type", "text", "password");
 		});
 	});
+
+	changeWord();
+	setInterval(changeWord, 4000);
 });
+
+var words = document.getElementsByClassName("welcome-title-content__word");
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+	splitLetters(words[i]);
+}
+
+function changeWord() {
+	var cw = wordArray[currentWord];
+	var nw =
+		currentWord == words.length - 1
+			? wordArray[0]
+			: wordArray[currentWord + 1];
+	for (var i = 0; i < cw.length; i++) {
+		animateLetterOut(cw, i);
+	}
+
+	for (var i = 0; i < nw.length; i++) {
+		nw[i].className = "letter behind";
+		nw[0].parentElement.style.opacity = 1;
+		animateLetterIn(nw, i);
+	}
+
+	currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
+}
+
+function animateLetterOut(cw, i) {
+	setTimeout(function () {
+		cw[i].className = "letter out";
+	}, i * 80);
+}
+
+function animateLetterIn(nw, i) {
+	setTimeout(function () {
+		nw[i].className = "letter in";
+	}, 340 + i * 80);
+}
+
+function splitLetters(word) {
+	var content = word.innerHTML;
+	word.innerHTML = "";
+	var letters = [];
+	for (var i = 0; i < content.length; i++) {
+		var letter = document.createElement("span");
+		letter.className = "letter";
+		letter.innerHTML = content.charAt(i);
+		word.appendChild(letter);
+		letters.push(letter);
+	}
+
+	wordArray.push(letters);
+}
