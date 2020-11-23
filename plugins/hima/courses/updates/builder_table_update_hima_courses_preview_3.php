ad4ad6eba66f23154e8eea3dfdc18c6526a443bc
ftp <webpar3z_mir@cartman.beget.ru>
2020-11-23 16:29:56 +0300

    course preview +materials

---
+++ plugins/hima/courses/updates/builder_table_update_hima_courses_preview_3.php
@@ -0,0 +1,23 @@
+<?php namespace Hima\Courses\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateHimaCoursesPreview3 extends Migration
+{
+    public function up()
+    {
+        Schema::table('hima_courses_preview', function($table)
+        {
+            $table->text('materials');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('hima_courses_preview', function($table)
+        {
+            $table->dropColumn('materials');
+        });
+    }
+}
