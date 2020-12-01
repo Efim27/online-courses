449842479962977ee23737c7bd8950b51b71de53
ftp <webpar3z_mir@cartman.beget.ru>
2020-12-01 06:00:29 +0300

    _plugin yandex kassa

---
+++ plugins/hima/courses/updates/builder_table_create_hima_courses_course_user.php
@@ -0,0 +1,22 @@
+<?php namespace Hima\Courses\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateHimaCoursesCourseUser extends Migration
+{
+    public function up()
+    {
+        Schema::create('hima_courses_course_user', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->integer('course_id');
+            $table->integer('user_id');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('hima_courses_course_user');
+    }
+}
---
+++ plugins/hima/yandexkassa/updates/builder_table_create_hima_yandexkassa_settings.php
@@ -0,0 +1,23 @@
+<?php namespace Hima\Yandexkassa\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateHimaYandexkassaSettings extends Migration
+{
+    public function up()
+    {
+        Schema::create('hima_yandexkassa_settings', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->string('secret_key');
+            $table->string('shop_id');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('hima_yandexkassa_settings');
+    }
+}
